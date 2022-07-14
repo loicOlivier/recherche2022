5b1168d211bcf2cda74565cfa82ce98804791a9e
loicOlivier <93949230+loicOlivier@users.noreply.github.com>
2022-07-14 11:02:44 -0400

    Debut tuto

---
+++ Tutoriel_Numpy.py
@@ -0,0 +1,11 @@
+# Lien  pour le tutoriel : https://www.youtube.com/watch?v=DcfYgePyedM&list=PLkdGijFCNuVnGxo-1fSNcdHh5gZc17oRM&index=5&ab_channel=Mr.PSolver
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+#Création d'un tableau numpy
+a1 = np.array([4,3,7,8])
+a2 = np.zeros(10)
+a3 = np.ones(10)
+
+print(a1)
